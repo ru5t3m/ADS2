7b6fe75af0e167a765646a5923d0e0b0a903cb4d
ru5t3m <zharmagambetovrustem@gmail.com>
2023-04-20 20:19:07 +0600

    first step

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -2,4 +2,59 @@ public class MyArrayList<T> implements MyList<T> {
     private Object[] elements;
     private int size;
     private static final int DEFAULT_CAPACITY = 10;
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
