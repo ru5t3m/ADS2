dd0ee1e3b5fd02db8aaa556b5d1ed64250855b06
ru5t3m <zharmagambetovrustem@gmail.com>
2023-04-20 22:49:19 +0600

    MyLinkedList with all head tail and size parameters

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -16,6 +16,11 @@ public class MyLinkedList<T> implements MyList<T> {
     private Node tail;
     private int size;
 
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
     @Override
     public int size() {
