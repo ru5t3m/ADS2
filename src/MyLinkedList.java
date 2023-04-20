f3ac49aa7c0b3e9bcdceb951915732ea550a5364
ru5t3m <zharmagambetovrustem@gmail.com>
2023-04-20 22:43:50 +0600

    Node and implements

---
+++ src/MyLinkedList.java
@@ -0,0 +1,71 @@
+public class MyLinkedList<T> implements MyList<T> {
+
+    private class Node {
+        T data;
+        Node next;
+        Node prev;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+    }
+}
