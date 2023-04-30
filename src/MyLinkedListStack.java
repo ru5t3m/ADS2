5983998676308b3fb53c945b8e66a446baf1b182
ru5t3m <zharmagambetovrustem@gmail.com>
2023-05-01 02:33:19 +0600

    const

---
+++ src/MyLinkedListStack.java
@@ -0,0 +1,9 @@
+import java.util.EmptyStackException;
+
+public class MyLinkedListStack<T> {
+    private MyLinkedList<T> stack;
+
+    public MyLinkedListStack() {
+        stack = new MyLinkedList<>();
+    }
+}
